268d079edd1d3aed89ebe2d228ca7cf67e16bcdb
Vincent <vincent@imac-de-vincent.local>
2016-09-09 14:15:21 +0200

    Ajout du tuto

--- driverManager/driverManager.cpp
+++ driverManager/driverManager.cpp
@@ -1,1 +1,43 @@
-/* add your code here */
+#include <IOKit/IOLib.h>
+#include "driverManager.hpp"
+
+// This required macro defines the class's constructors, destructors,
+// and several other methods I/O Kit requires.
+OSDefineMetaClassAndStructors(com_ComposJuliot_driver_driverManager, IOService)
+
+// Define the driver's superclass.
+#define super IOService
+
+bool com_ComposJuliot_driver_driverManager::init(OSDictionary *dict)
+{
+    bool result = super::init(dict);
+    IOLog("Initializing\n");
+    return result;
+}
+
+void com_ComposJuliot_driver_driverManager::free(void)
+{
+    IOLog("Freeing\n");
+    super::free();
+}
+
+IOService *com_ComposJuliot_driver_driverManager::probe(IOService *provider,
+                                                SInt32 *score)
+{
+    IOService *result = super::probe(provider, score);
+    IOLog("Probing\n");
+    return result;
+}
+
+bool com_ComposJuliot_driver_driverManager::start(IOService *provider)
+{
+    bool result = super::start(provider);
+    IOLog("Starting\n");
+    return result;
+}
+
+void com_ComposJuliot_driver_driverManager::stop(IOService *provider)
+{
+    IOLog("Stopping\n");
+    super::stop(provider);
+}
--- driverManager/driverManager.hpp
+++ driverManager/driverManager.hpp
@@ -1,1 +1,11 @@
-/* add your code here */
+#include <IOKit/IOService.h>
+class com_ComposJuliot_driver_driverManager : public IOService
+{
+    OSDeclareDefaultStructors(com_ComposJuliot_driver_driverManager)
+public:
+    virtual bool init(OSDictionary *dictionary = 0);
+    virtual void free(void);
+    virtual IOService *probe(IOService *provider, SInt32 *score);
+    virtual bool start(IOService *provider);
+    virtual void stop(IOService *provider);
+};
